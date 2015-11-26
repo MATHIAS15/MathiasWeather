b0706ea19097ce438b976123971ab2ebe79de9d9
nozkrew <thomas.maire1@hotmail.fr>
2015-11-26 10:08:36 +0100

    Initial commit

---
+++ Weather_Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather;
+
+namespace Weather_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
